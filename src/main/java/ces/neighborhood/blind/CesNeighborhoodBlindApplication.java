c1143a17c0176208144af82d68a9b2d14838e601
the-mascot <dmstn1812@naver.com>
2023-11-14 15:18:57 +0900

    프로젝트 생성

---
+++ src/main/java/ces/neighborhood/blind/CesNeighborhoodBlindApplication.java
@@ -0,0 +1,13 @@
+package ces.neighborhood.blind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesNeighborhoodBlindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesNeighborhoodBlindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ces/neighborhood/blind/CesNeighborhoodBlindApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.neighborhood.blind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesNeighborhoodBlindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
