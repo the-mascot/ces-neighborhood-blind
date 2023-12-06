d6b0edae22a2686197f992ed853cc8a8dd338a19
mascot <dmstn1812@naver.com>
2023-12-06 18:01:01 +0900

    소셜로그인 구현 진행

--- src/main/java/ces/neighborhood/blind/common/filter/AuthenticationFilter.java
+++
@@ -1,9 +0,0 @@
-package ces.neighborhood.blind.common.filter;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-
-
-}
