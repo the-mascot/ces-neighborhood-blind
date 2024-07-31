790f161c2255440ee43197f618f5525702d03df2
mascot <dmstn1812@naver.com>
2024-07-31 18:38:43 +0900

    메뉴 관련 코드 제거

--- src/main/java/ces/neighborhood/blind/app/controller/IndexController.java
+++ src/main/java/ces/neighborhood/blind/app/controller/IndexController.java
@@ -4,12 +4,14 @@ import ces.neighborhood.blind.app.dto.ApiResponse;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+@RequestMapping("/api/v1")
+public class IndexController {
 
-    @GetMapping
+    @GetMapping("/index")
     public ResponseEntity getMenu() {
 
 
